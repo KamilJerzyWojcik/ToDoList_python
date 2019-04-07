7f59f90511c15614a628788653f9191a591c748b
KarolinaCeteraWojcik <karolina.cetera.wojcik@gmail.com>
2019-04-07 13:03:36 +0200

    Initial2

--- venvMac/bin/activate.fish
+++ venvMac/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shrewfallout/Projects/Python tutorial/ToDoList/venvMac"
+set -gx VIRTUAL_ENV "/Users/shrewfallout/Projects/Python tutorial/ToDoList_python/venvMac"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
